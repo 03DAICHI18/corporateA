ec8204e30d72382ea5e27c900b8e9ec1f46a0c31
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-11-13 18:41:28 +0900

    初回コミット

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function() {
+
+// (必要であれば)画面リロード時に一旦非表示設定をしておく
+$(window).on('load resize', function(){
+  $('#page-top').css('display','none');
+  });
+
+// 表示位置がトップからどれくらいに位置しているか
+  $(window).scroll(function () {
+  if ($(this).scrollTop() < 200) {
+// 200px以下だとフェードアウト
+   $('#page-top').fadeOut();
+    } else {
+// 200px以上離れたらフェードイン
+   $('#page-top').fadeIn();
+    }
+    });
+// クリックしたらトップ（高さ0px）へスクロール（0.5秒かけて）
+   $('#page-top').click(function () {
+   $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+    });
+
+  });
